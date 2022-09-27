eaebfb6b8c6c9869c34ff03ebd9b6e55693c994c
Skylan Lew <skylanslew@gmail.com>
2022-09-27 14:10:10 -0700

    working prototype
    Co-authored-by: Ted Gold <ted.jonathan.gold@gmail.com>

--- Parcels/Controllers/ParcelsController.cs
+++ Parcels/Controllers/ParcelsController.cs
@@ -1,13 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
+using Parcels.Models;
+using System.Collections.Generic;
+
 
 namespace Parcels.Controllers
 {
     public class ParcelsController : Controller
     {
-      [HttpGet("/")]
+      [HttpGet("/parcels")]
       public ActionResult Index()
       {
+        List<Parcel> allParcels = Parcel.GetAll();
+        return View(allParcels);
+      }
+
+      [HttpGet("/parcels/new")]
+      public ActionResult ParcelForm()
+      {
         return View();
+      }
+
+      [HttpPost("/parcels")]
+      public ActionResult Create(string name, int height, int width, int depth, int weight)
+      {
+        Parcel myParcel = new Parcel(name, height, width, depth, weight);
+        return RedirectToAction("Index");
       }
     }
 }
--- Parcels/Models/Parcel.cs
+++ Parcels/Models/Parcel.cs
@@ -1,7 +1,47 @@
+using System.Collections.Generic;
+
 namespace Parcels.Models
 {
   public class Parcel
   {
+    public string Name { get; set; }
+    public int Height { get; set; }
+    public int Width { get; set; }
+    public int Depth { get; set; }
+    public int Weight { get; set; }
 
+    public static List<Parcel> _instances = new List<Parcel>{};
+
+    public Parcel(string name, int height, int width, int depth, int weight)
+    {
+      Name = name;
+      Height = height;
+      Width = width;
+      Depth = depth;
+      Weight = weight;
+      _instances.Add(this);
+    }
+
+    public static List<Parcel> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public int Volume()
+    {
+      int volume = Height * Width * Depth;
+      return volume;
+    }
+
+    public float CostToShip()
+    {
+      float costToShip = 5;
+      return costToShip;
+    }
   }
 }
